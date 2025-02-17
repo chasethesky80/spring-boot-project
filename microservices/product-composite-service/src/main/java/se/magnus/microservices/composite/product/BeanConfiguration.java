dc1bd62d9b4824dc50ee1f3dc3fa983d32d323e0
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-16 18:32:10 -0600

    More changes

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/BeanConfiguration.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/BeanConfiguration.java
@@ -3,13 +3,9 @@ package se.magnus.microservices.composite.product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import se.magnus.util.http.ServiceUtil;
 
 @Configuration
 public class BeanConfiguration {
 
-    @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+   
 }
